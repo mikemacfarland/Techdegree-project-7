d50139aa25390f7a284a6c72ee40c244c4edb7a2
Mike Macfarland <m_macfarland@live.com>
2021-05-13 19:01:51 -0600

    updated SVGs, updated some styles for list items and text size on media queries

--- js/charts.js
+++ js/charts.js
@@ -237,6 +237,8 @@ const mobileOptions = {
 // USER MESSAGE FORM
 // ------------------------
 
+
+
 // send button event listener
 send.addEventListener('click', () => {
   if (searchUser.value === "" && messageUser.value === ""){
@@ -255,6 +257,39 @@ send.addEventListener('click', () => {
     alert(`Message sent to ${searchUser.value}`)
   }
 })
+
+
+// user search
+const userNames = [ "Dale Byrd",
+                    "Victoria Chambers",
+                    "Dawn Wood", 
+                    "Dan Oliver"]
+
+/*list of available options*/
+var n = userNames.length; //length of datalist tags    
+  
+function ac(value) {
+   document.getElementById('datalist').innerHTML = '';
+    //setting datalist empty at the start of function
+    //if we skip this step, same name will be repeated
+      
+    l = value.length;
+    //input query length
+for (var i = 0; i<n; i++) {
+    if(((userNames[i].toLowerCase()).indexOf(value.toLowerCase()))>-1)
+    {
+        //comparing if input string is existing in tags[i] string
+
+        var node = document.createElement("option");
+        var val = document.createTextNode(userNames[i]);
+         node.appendChild(val);
+
+          document.getElementById("datalist").appendChild(node);
+              //creating and appending new elements in data list
+    }
+  }
+}
+
 
 // ------------------------
 // SETTINGS WIDGET
@@ -318,4 +353,3 @@ settings.addEventListener('click', (e) => {
     }
   }
 })
-
